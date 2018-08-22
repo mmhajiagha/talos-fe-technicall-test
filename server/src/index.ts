4e912cc9ea142b0fcb93ecaa4a89ae09e4e3f67f
Mateo Carvajal <mateo.carvajal@talosdigital.com>
2018-08-22 13:50:26 -0500

    initial setup and server

---
+++ server/src/index.ts
@@ -0,0 +1,46 @@
+import * as express from 'express';
+import { HttpError, Server } from 'typescript-rest';
+import './controller/postsController';
+
+let app: express.Application = express();
+app.use(
+  (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  }
+);
+Server.buildServices(app);
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    if (err instanceof HttpError) {
+      res.set('Content-Type', 'application/json');
+      res.status(err.statusCode);
+      res.json({ data: { info: err.message, status: err.statusCode } });
+    } else {
+      next(err);
+    }
+  }
+);
+
+app.use(express.static('./public'));
+
+Server.swagger(app, './test/data/swagger.yaml', '/api-docs', 'localhost:3000', [
+  'http',
+]);
+
+app.listen(3000, function() {
+  console.log('Rest Server listening on port 3000!');
+});
