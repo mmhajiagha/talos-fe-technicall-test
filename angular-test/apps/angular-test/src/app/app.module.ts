b3e235cf7defbe325959ed7d96c55eebea970baf
mmhajiagha <mmhajiagha@gmail.com>
2021-01-09 15:11:26 +0330

    feat: add material and fix some dependencies versions

--- angular-test/apps/angular-test/src/app/app.module.ts
+++ angular-test/apps/angular-test/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+const MATERIAL_MODULES = [
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ...MATERIAL_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
