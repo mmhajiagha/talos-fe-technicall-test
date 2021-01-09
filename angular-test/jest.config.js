4755f53cda84b7d2e79b6b963ab41423afe16ac3
mmhajiagha <mmhajiagha@gmail.com>
2021-01-09 15:47:22 +0330

    feat: add common models and services libs

--- angular-test/jest.config.js
+++ angular-test/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/libs/angular-test/core'],
+  projects: [
+    '<rootDir>/libs/angular-test/core',
+    '<rootDir>/libs/app-models/common',
+    '<rootDir>/libs/app-services/common',
+  ],
 };
---
+++ angular-test/libs/app-services/common/README.md
@@ -0,0 +1,7 @@
+# app-services-common
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test app-services-common` to execute the unit tests.
